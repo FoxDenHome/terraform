c7ae2d6c6cd1b7a629cea3affeb5faa0b906ec67
Doridian <git@doridian.net>
2022-09-02 16:19:52 -0700

    allow domains without vanity NS

--- domains/domains.tf
+++ domains/domains.tf
@@ -101,6 +101,6 @@ module "domain" {
   redirect_www_to_root = each.value.redirect_www_to_root
   add_www_cname        = each.value.add_www_cname
   transfer_lock        = each.value.transfer_lock
-  vanity_nameserver    = constellix_vanity_nameserver.vanity[each.value.vanity_nameserver]
+  vanity_nameserver    = each.value.vanity_nameserver != null ? constellix_vanity_nameserver.vanity[each.value.vanity_nameserver] : null
   domain_contact       = var.domain_contact
 }
