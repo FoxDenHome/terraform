16e0428e0bf17d0b9433aeb433dd41c3d4296f6d
Doridian <git@doridian.net>
2022-09-14 09:20:31 -0700

    this should ignore everything we need to

--- github/modules/repo/archived/main.tf
+++ github/modules/repo/archived/main.tf
@@ -3,4 +3,19 @@ resource "github_repository" "repo" {
 
   archive_on_destroy = true
   archived           = true
+
+  lifecycle {
+    ignore_changes = [
+      allow_merge_commit,
+      allow_rebase_merge,
+      allow_squash_merge,
+      description,
+      has_issues,
+      has_wiki,
+      merge_commit_message,
+      merge_commit_title,
+      squash_merge_commit_message,
+      squash_merge_commit_title,
+    ]
+  }
 }
