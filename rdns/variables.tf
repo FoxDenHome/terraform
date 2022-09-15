e16f0f8381516c77161f282dae258fa1ae371b50
Doridian <git@doridian.net>
2022-09-15 07:58:46 -0700

    rm useless

--- rdns/variables.tf
+++ rdns/variables.tf
@@ -1,14 +1,6 @@
 variable "domains" {
   type = map(object({
-    fastmail = bool
-    ses      = bool
-
-    add_root_aname = bool
-    add_www_cname  = bool
-
     vanity_nameserver = string
-
-    extra_attributes = map(string)
   }))
   default = {}
 }
