fe421cec1954ed8fce7a23278b3ab96ebad2e017
Doridian <git@doridian.net>
2023-08-03 00:08:58 -0700

    this is not supported atm, bt

--- domains/f0x_es.tf
+++ domains/f0x_es.tf
@@ -15,3 +15,25 @@ resource "cloudns_dns_record" "f0x_es_vixus" {
   ttl   = 3600
   value = "arcticfox.doridian.net"
 }
+
+local {
+  xmpp_ports = {
+    "_xmpp-client._tcp" = 5222,
+    "_xmpp-server._tcp" = 5269,
+  }
+}
+
+# resource "cloudns_dns_record" "f0x_es_jabber" {
+#   for_each = local.xmpp_ports
+#   zone     = "f0x.es"
+
+#   type = "SRV"
+#   name = each.key
+#   port = each.value
+
+#   value    = "redfox.doridian.net"
+#   priority = 10
+#   weight   = 10
+
+#   ttl = 3600
+# }
