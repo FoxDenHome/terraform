03a7f676ba60b5f049bbfccde39eb02454451a17
Doridian <git@doridian.net>
2023-11-04 10:35:05 -0700

    rm stuff

--- domains/f0x_es.tf
+++ domains/f0x_es.tf
@@ -7,21 +7,12 @@ resource "cloudns_dns_record" "f0x_es_c0de" {
   value = "c0defox.es"
 }
 
-resource "cloudns_dns_record" "f0x_es_vixus" {
-  zone = "f0x.es"
-
-  type  = "CNAME"
-  name  = "vixus"
-  ttl   = 3600
-  value = "arcticfox.doridian.net"
-}
-
-locals {
-  xmpp_ports = {
-    "_xmpp-client._tcp" = 5222,
-    "_xmpp-server._tcp" = 5269,
-  }
-}
+# locals {
+#   xmpp_ports = {
+#     "_xmpp-client._tcp" = 5222,
+#     "_xmpp-server._tcp" = 5269,
+#   }
+# }
 
 # resource "cloudns_dns_record" "f0x_es_jabber" {
 #   for_each = local.xmpp_ports
@@ -37,17 +28,3 @@ locals {
 
 #   ttl = 3600
 # }
-
-locals {
-  xmpp_subdomains = toset(["xmppshare", "telegram", "discord"])
-}
-
-resource "cloudns_dns_record" "f0x_es_xmppshare" {
-  for_each = local.xmpp_subdomains
-  zone     = "f0x.es"
-
-  type  = "CNAME"
-  name  = each.key
-  ttl   = 3600
-  value = "redfox.doridian.net"
-}
