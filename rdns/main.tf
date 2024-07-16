dbcab70b6348c93efbf98031ce6f629176743152
Doridian <git@doridian.net>
2024-07-16 15:06:23 -0700

    swap over

--- rdns/main.tf
+++ rdns/main.tf
@@ -4,15 +4,19 @@ locals {
     "0.f.4.4.d.7.e.0.a.2.ip6.arpa" = {},
     // RIPE /44
     "c.1.2.2.0.f.8.e.0.a.2.ip6.arpa" = {},
-    // Tunnelbroker IPv6 /64
-    "9.3.0.0.b.0.0.0.0.7.4.0.1.0.0.2.ip6.arpa" = {},
-    // Tunnelbroker IPv6 /48
-    "0.2.8.e.0.7.4.0.1.0.0.2.ip6.arpa" = {},
   }, var.domains)
 
   foxden_home_rdns = [
     {
       subzone = "9.6",
+      zone    = "0.f.4.4.d.7.e.0.a.2.ip6.arpa",
+    },
+    {
+      subzone = "a.0",
+      zone    = "0.f.4.4.d.7.e.0.a.2.ip6.arpa",
+    },
+    {
+      subzone = "b.0",
       zone    = "0.f.4.4.d.7.e.0.a.2.ip6.arpa",
     },
   ]
